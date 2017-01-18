85051a4151fd3b6409c125c092377f140f2fe872
William Durand <will+git@drnd.me>
2017-01-18 01:27:45 +0100

    Ultra minimal style + get rid of useless things

--- app/models/User.js
+++ app/models/User.js
@@ -8,13 +8,7 @@ const userSchema = new mongoose.Schema({
   passwordResetToken: String,
   passwordResetExpires: Date,
 
-  facebook: String,
-  twitter: String,
-  google: String,
   github: String,
-  instagram: String,
-  linkedin: String,
-  steam: String,
   tokens: Array,
 
   profile: {
