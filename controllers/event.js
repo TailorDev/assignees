db649f9665042e51b40b8ff8a282f36f8aa88e0d
William Durand <will+git@drnd.me>
2017-01-19 18:18:48 +0100

    Finally respond to GH events

--- controllers/event.js
+++ controllers/event.js
@@ -1,8 +1,12 @@
+const gh = require('../helpers/github');
+const User = require('../models/User');
+const Repository = require('../models/Repository');
+
 /**
  * Listen to GitHub events
  */
 exports.listen = (req, res) => {
-  if (req.header('x-github-event') !== 'ping') {
+  if (req.header('x-github-event') === 'ping') {
     return res.send('PONG');
   }
 
@@ -14,9 +18,45 @@ exports.listen = (req, res) => {
     return res.send({ status: 'ignored', reason: 'Action is not "opened".' });
   }
 
-  // TODO: see if project is still enabled
-  // TODO: send the PR to queue for assigning reviewers
-  // TODO: see if `req.body.pull_request.title` contains WIP
+  // TODO: check params
 
-  res.send({ status: 'accepted' });
+  const repoId = req.body.repository.id;
+  const pullTitle = req.body.pull_request.title;
+  const pullNumber = req.body.pull_request.number;
+
+  Repository.findOne({ github_id: repoId }, (err, repository) => {
+    if (!repository) {
+      return res.send({ status: 'ignored', reason: 'Unknown repository.' });
+    }
+
+    if (!repository.enabled) {
+      return res.send({ status: 'ignored', reason: 'Repository is paused.' });
+    }
+
+    if (repository.skip_wip && /wip/i.test(pullTitle)) {
+      return res.send({ status: 'ignored', reason: 'WIP.' });
+    }
+
+    // TODO: find collaborators, also check `max_reviewers`
+    // TODO: move this logic to a worker
+
+    User.findOne({ _id: repository.user_id }, (err, user) => {
+      gh.auth(user).pullRequests
+        .createReviewRequest({
+          owner: repository.org,
+          repo: repository.name,
+          number: pullNumber,
+          reviewers: ['jmaupetit'],
+        })
+        .then(() => {
+          res.send({ status: 'accepted' });
+        })
+        .catch((err) => {
+          console.log({err});
+
+          res.send({ status: 'errored' });
+        })
+      ;
+    });
+  });
 };
