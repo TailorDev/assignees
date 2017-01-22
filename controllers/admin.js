dbd464e0ff9e47a3dc106ec8fb11237f23758ef5
William Durand <will+git@drnd.me>
2017-01-22 21:30:10 +0100

    Add more metrics to dashboard

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,14 +4,21 @@ const User = require('../models/User');
 /**
  * Dashboard
  */
-exports.index = (req, res) => {
-  Repository.count({}, (err, nbRepositories) => {
-    User.count({}, (err, nbUsers) => {
-      res.render('dashboard/index', {
-        title: 'Dashboard',
-        nbRepositories,
-        nbUsers,
-      });
-    });
+exports.index = async (req, res) => {
+  const nbRepositories = await Repository.count()
+    .catch(err => null);
+  const nbEnabledRepos = await Repository.count({ enabled: true, })
+    .catch(err => null);
+  const nbUsers = await User.count()
+    .catch(err => null);
+  const nbPrivateUsers = await User.count({ 'tokens.scopes': { $in: ['repo'] } })
+    .catch(err => null);
+
+  return res.render('dashboard/index', {
+    title: 'Dashboard',
+    nbRepositories,
+    nbUsers,
+    nbPrivateUsers,
+    nbEnabledRepos,
   });
 };
