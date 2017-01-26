e14a86eb1a22dc97eb4621857ff6ffbb9fac99d6
William Durand <will+git@drnd.me>
2017-01-26 22:51:06 +0100

    Avoid to cache bust on every deploy

--- middlewares/assets.js
+++ middlewares/assets.js
@@ -1,6 +1,8 @@
+const pkg = require('../package.json');
+
 module.exports = () => {
-  if (process.env.NODE_ENV === 'production') {
-    const version = Date.now();
+  if (process.env.NODE_ENV !== 'production') {
+    const version = pkg.version;
 
     return (url) => {
       return `${url}?v=${version}`;
